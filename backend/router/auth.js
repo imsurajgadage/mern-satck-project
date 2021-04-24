05db7a2fa9c3368e8325d411aab91d041b93f1cc
imsurajgadage <surajgadage20@gmail.com>
2021-04-24 22:59:19 +0530

    Storing the user data at online DB using Promises

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -2,14 +2,33 @@ const express = require('express')
 
 const router = express.Router();
 
+require('../db/conn');
+const User = require("../model/userSchema")
 
 router.get("/",(req,res)=>{
     res.send("Hello from Home Page")
 })
 
 router.post("/register", (req,res)=>{
-    console.log(req.body)
-    res.json({message: req.body})
+    const {name, email, phone,work,password,cpassword} = req.body
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error: 'Fill The All Fields!'})
+    }
+
+    User.findOne({email:email})
+    .then((userExits)=>{
+        if(userExits){
+            return res.status(422).json({error: 'Email Already Exits!'})
+        }
+
+        const user = new User({name, email, phone,work,password,cpassword});
+
+        user.save().then(()=>{
+            res.status(201).json({message: 'Registration Successful!'})
+        })
+        .catch((err)=> res.status(500).json({error: 'Failed to register'}))
+        
+    }).catch((err)=> {console.log(err)})
 })
 
 
