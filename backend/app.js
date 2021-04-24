c6a4c94ab09ba407bda5170d99cdba263a0a677a
imsurajgadage <surajgadage20@gmail.com>
2021-04-24 08:39:05 +0530

    Added MiddleWare

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,31 @@ const express = require('express');
 
 const app = express();
 
+// Middleware
+
+const middleware = (req,res,next) =>{
+    console.log(`Hello muy Middleware`)
+    next()
+}
+
 app.get("/",(req,res)=>{
     res.send("Hello from Home Page")
 })
 
-app.get("/about",(req,res)=>{
+app.get("/about", middleware, (req,res)=>{
     res.send("Hello from About Page")
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("Hello from Contact Page")
+})
+
+app.get("/signin",(req,res)=>{
+    res.send("Hello from Signin Page")
+})
+
+app.get("/signup",(req,res)=>{
+    res.send("Hello from Signup Page")
 })
 
 
